e1da0316a0daf684795b50880047edaefabf2e59
Podkopaev Anton <podkoav239@gmail.com>
2015-01-20 15:44:57 +0300

    Added L parser

--- lInt/Program.fs
+++ lInt/Program.fs
@@ -6,12 +6,13 @@ module CoreParser =
   let (>>=) (p: 'a t) (f: 'a -> 'b t) = fun s ->
     List.concat [for (r, s') in p s -> (f r) s']
 
-  let (>>) p q = p >>= fun _ -> q
-  
   let mreturn r = fun s -> [(r, s)]
   let lambda    = fun s -> []
   let item      = fun s -> match s with [] -> [] | h :: s -> [(h, s)]
   let sat cond  = item >>= fun c -> if cond c then mreturn c else lambda
+  
+  let (>>) p q = p >>= fun _ -> q
+  let (<<) p q = p >>= fun rs -> q >> mreturn rs
   
   let char c    = sat ((=) c)
   let digit     = sat (fun c -> (List.tryFind ((=) c) ['0'..'9']).IsSome) 
@@ -38,13 +39,26 @@ module CoreParser =
   let map (f: 'a -> 'b) (p: 'a t): 'b t = fun s ->
     p s |> List.map (fun (e, s) -> (f e, s)) 
   
-  let number  = map (fun s -> %s |> System.Int32.Parse) (many1 digit) 
-  let word    = map (~%) (many1 alpha)
-  let space f = many0 (char ' ') >> f
+  let number = map (fun s -> %s |> System.Int32.Parse) (many1 digit) 
+  let word   = map (~%) (many1 alpha)
+  let spaces = many0 (char ' ')
+  let sp   f = spaces >> f << spaces 
+  
+  let paren p =
+    let lparen = char '('
+    let rparen = char ')'
+    sp lparen >>= (fun _ -> p() << sp rparen)
+  let paren' p = paren (fun _ -> p)  
+  
+  let cparen p =
+    let lcparen = char '{'
+    let rcparen = char '}'
+    sp lcparen >>= (fun _ -> p() << sp rcparen)
+  let cparen' p = cparen (fun _ -> p)  
 
 // ---------
 
-module Calculator =
+module Expr =
   type t = Num of int
          | Var of string
          | BinOp of char * t * t
@@ -58,39 +72,90 @@ module Calculator =
     | '/' -> (/)
     | _   -> raise NotOperation
 
-  module Lexer =
+  module Parser =
     open CoreParser
-    
-    type t = LParen | RParen | Num of int | Var of string | BinOp of char
-    
+
     exception EmptyList
     let fold1 f s =
       match s with
       | h :: s -> List.fold f h s
       | [] -> raise EmptyList
 
-    let l: (t list) CoreParser.t =
-      let paren c s = map (fun _ -> c) (char s)
-      let lparen = paren LParen '('
-      let rparen = paren RParen ')'
-      let n      = map Num number
-      let v      = map Var word
-      let b      = ['+'; '-'; '*'; '/'] |>
-                   List.map char |>
-                   fold1 (<|>) |>
-                   map BinOp
-      (lparen <|> rparen <|> n <|> v <|> b) |> space |> many0
- 
-  module Parser =
-    let parse s =
-      let lexems = s |> Lexer.l
-      "asdf"
+    let binOp  = ['+'; '-'; '*'; '/'] |>
+                 List.map char |>
+                 fold1 (<|>)
+    
+    let term = (map Num number) <|> (map Var word) |> sp
+    let binOpP p = term >>= fun leftArg ->
+                  sp binOp >>= fun op ->
+                  map (fun s -> BinOp (op, leftArg, s)) (p()) 
 
-open Calculator.Lexer
+    let rec parse _: t CoreParser.t =
+      binOpP parse <|> term <|> paren parse
+
+  let (>>=) a f =
+    match a with
+    | None -> None
+    | Some  a -> f a 
+  let mreturn a = Some a
+
+  let rec calc env e =
+    match e with
+    | Num n -> Some n
+    | Var x -> env  x
+    | BinOp (o, x, y) ->
+      calc env x >>= fun vx -> 
+      calc env y >>= fun vy -> 
+      (op o) vx vy |> mreturn 
+
+module Interpreter =
+  type t = Read   of string
+         | Write  of Expr.t
+         | Assign of string * Expr.t
+         | Seq    of t * t
+         | If     of Expr.t * t * t
+         | While  of Expr.t * t
+
+  open CoreParser
+  let readP   = symbol &"read"  >> paren' (map (fun x -> Read x) word)
+  let writeP  = symbol &"write" >> paren' (map Write (Expr.Parser.parse ()))
+  let assignP = (sp word << sp (symbol &":=")) >>= fun x ->
+                map (fun e -> Assign (x, e)) (Expr.Parser.parse ())
+
+  let ifP    p = (symbol &"if" |> sp)  >>
+                 (Expr.Parser.parse () |> sp) >>= fun e ->
+                 p() |> cparen' >>= fun op1 ->
+                 (symbol &"else" |> sp) >>
+                 (p() |> cparen' |> map (fun op2 -> If (e, op1, op2)))
+  let whileP p = (symbol &"while" |> sp)  >>
+                 (Expr.Parser.parse () |> sp) >>= fun e ->
+                 (p() |> cparen' |> map (fun op -> While (e, op)))
+  
+  let term   p = readP <|> writeP <|> assignP <|> ifP p <|> whileP p
+  
+  let seqP   p = (term p << sp (char ';')) >>= fun x ->
+                 (p() |> sp |> map (fun y -> Seq (x, y)))
+  
+  let rec parse _: t CoreParser.t =
+    seqP parse <|> term parse
+
+
+open Expr.Parser
 open CoreParser
 
 [<EntryPoint>]
 let main argv =
-  let s = "(5 + x)"
-  printfn "%A" (l &s)
+  let s1 = &"while (x + 5) { read(y); write(x) }"
+  let e1 = Interpreter.parse () s1
+  printfn "%A" e1
+(*
+  let s1 = &"(5 + 5 - (x + 7))"
+  let e1 = parse () s1
+  let s2 = &"(5 + 5 - (y + 7))"
+  let e2 = parse () s2
+  let f e = e |> List.map (fun (x, _) -> x)
+              |> List.map (Expr.calc (fun x -> if x = "x" then Some 5 else None))
+              |> printfn "%A"
+  f e1
+  f e2*)
   0 // return an integer exit code
